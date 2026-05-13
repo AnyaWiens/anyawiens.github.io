e95c019f4ea8a91711b4b2c6d77755468f411d5a
AnyaWiens <46979056+AnyaWiens@users.noreply.github.com>
2026-05-13 17:43:21 -0600

    remove print statements

--- guild_leaderboard/README.md
+++
@@ -1,3 +0,0 @@
-# guild_leaderboard
-
-A new Flutter project.
--- guild_leaderboard/android/app/src/main/kotlin/com/example/guild_leaderboard/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.guild_leaderboard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
