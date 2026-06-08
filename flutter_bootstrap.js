7658839cb2d7cac7ecbfa990595d5060fca64fa4
AnyaWiens <46979056+AnyaWiens@users.noreply.github.com>
2026-06-08 09:38:27 -0600

    removed message exchange connectors

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1897444159" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4234769268" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
